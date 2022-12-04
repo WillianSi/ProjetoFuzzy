b37c19abebc80591913ddd04cb714339305e1bcf
Willian Silvestre <70384084+WillianSi@users.noreply.github.com>
2022-12-03 21:52:00 -0300

    v1

---
+++ src/fuzzy/Fuzzy.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fuzzy;
+
+/**
+ *
+ * @author 11615476652
+ */
+public class Fuzzy {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FrmjFuzzyLogic form = new FrmjFuzzyLogic();
+        form.setVisible(true);
+    }
+    
+}
